e3247354ea829f497d81a79ce03a49e0127f6a28
aziz <azizboukardougha.ab@gmail.com>
2025-10-01 13:33:21 +0100

    base scout update 2/2.1

--- src/Chat/base-scout/base-scout.module.ts
+++ src/Chat/base-scout/base-scout.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BaseScoutService } from './base-scout.service';
 import { BaseScoutController } from './base-scout.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from 'src/models/ProductSchema';
 
 @Module({
   controllers: [BaseScoutController],
   providers: [BaseScoutService],
+    imports:[    
+      MongooseModule.forFeature([
+          { name: Product.name, schema: ProductSchema },
+        ]),]
 })
 export class BaseScoutModule {}
