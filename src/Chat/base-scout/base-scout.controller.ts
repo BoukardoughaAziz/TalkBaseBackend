2d3a8378219588ea1b36139cb736976d8d710240
aziz <azizboukardougha.ab@gmail.com>
2025-10-01 02:48:23 +0100

    updated the app modeule file :p

--- src/Chat/base-scout/base-scout.controller.ts
+++ src/Chat/base-scout/base-scout.controller.ts
@@ -1,12 +1,10 @@
 import { BaseScoutService } from './base-scout.service';
 import { Body, Controller, Post } from '@nestjs/common';
-import { ChatMessage } from '../../models/ChatMessageSchema';
+import { ChatMessage } from '../models/ChatMessageSchema';
 
 @Controller('base-scout')
 export class BaseScoutController {
   constructor(private readonly baseScoutService: BaseScoutService) {}
-
-
     @Post('scout')
   async askQuestion(@Body() body: { productLink: string; }) {
     console.log("THE LINK IS  --Controller",body.productLink);
--- src/app-module.ts
+++ src/app-module.ts
@@ -7,13 +7,14 @@ import { LiveKitModule } from './LiveKit/LiveKitModule';
  import { ClientInformationModule } from './client-information/client-information.module';
 import { ConversationModule } from './conversation/conversation.module';
 import { BaseBuddyModule } from './Chat/base-buddy/base-buddy.module';
+import {  } from './Chat/base-scout/base-scout.module';
  
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://azizboukardougha:N4TMDDmbsefVd8fd@cluster0.ojgrhfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),//Nwidget?replicaSet=rs0
     CallCenterAuthModule,
-    ChatModule, LiveKitModule,StatModule, ClientInformationModule,ConversationModule,BaseBuddyModule
+    ChatModule, LiveKitModule,StatModule, ClientInformationModule,ConversationModule,BaseBuddyModule,BaseScoutModule
   ], 
   controllers: [], 
   providers: [],
